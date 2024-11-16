4baceb5558ec80ac1c77eb5fe9b7128c8f9b0cfc
Mykola <shanikolay@gmail.com>
2024-11-16 19:48:55 +0200

    Add project files.

---
+++ Calculator_app/Program.cs
@@ -0,0 +1,49 @@
+﻿// Калькулятор, для арифметичних операцій з двома числами
+
+// Ініціалізація даних
+Console.Write("Введiть перше число: ");
+int first_number = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введiть друге число: ");
+int second_number = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введiть одну з арифметичних операцiй (+, -, *, /): ");
+string? math_operator = Console.ReadLine();
+
+// Виведення результату
+Console.Write($"{first_number} {math_operator} {second_number} = ");
+
+// Обчислення результату
+switch (math_operator)
+{
+    case "+":
+        Console.WriteLine(first_number + second_number);
+        break;
+    case "-":
+        Console.WriteLine(first_number - second_number);
+        break;
+    case "*":
+        Console.WriteLine(first_number * second_number);
+        break;
+    case "/":
+        // Перевірка ділення на ноль
+        if(second_number == 0)
+        {
+            Console.WriteLine("Error");
+            Console.WriteLine("На ноль дiлити не можна");
+        }
+        // Перевірка ділення без залишку
+        else if (first_number % second_number != 0)
+        {
+            Console.WriteLine(Convert.ToDouble(first_number) / Convert.ToDouble(second_number));
+        }
+        else
+        {
+            Console.WriteLine(first_number / second_number);
+        }
+        break;
+    default:
+        Console.WriteLine("Error");
+        Console.WriteLine("Оператор введено не вiрно!");
+        break;
+}
