19e8575b15efcf4930bf939ee0af47f8538809f3
Mykola <shanikolay@gmail.com>
2024-11-20 22:16:32 +0200

    Exception

--- Calculator_app/Program.cs
+++ Calculator_app/Program.cs
@@ -1,49 +1,74 @@
-﻿// Калькулятор, для арифметичних операцій з двома числами
+﻿using System.Text;
 
-// Ініціалізація даних
-Console.Write("Введiть перше число: ");
-int first_number = Convert.ToInt32(Console.ReadLine());
+Console.OutputEncoding = Encoding.UTF8;
 
-Console.Write("Введiть друге число: ");
-int second_number = Convert.ToInt32(Console.ReadLine());
+// Калькулятор, для арифметичних операцій з двома числами
 
-Console.Write("Введiть одну з арифметичних операцiй (+, -, *, /): ");
-string? math_operator = Console.ReadLine();
-
-// Виведення результату
-Console.Write($"{first_number} {math_operator} {second_number} = ");
-
-// Обчислення результату
-switch (math_operator)
+try
 {
-    case "+":
-        Console.WriteLine(first_number + second_number);
-        break;
-    case "-":
-        Console.WriteLine(first_number - second_number);
-        break;
-    case "*":
-        Console.WriteLine(first_number * second_number);
-        break;
-    case "/":
-        // Перевірка ділення на ноль
-        if(second_number == 0)
-        {
-            Console.WriteLine("Error");
-            Console.WriteLine("На ноль дiлити не можна");
-        }
-        // Перевірка ділення без залишку
-        else if (first_number % second_number != 0)
-        {
-            Console.WriteLine(Convert.ToDouble(first_number) / Convert.ToDouble(second_number));
-        }
-        else
-        {
-            Console.WriteLine(first_number / second_number);
-        }
-        break;
-    default:
-        Console.WriteLine("Error");
-        Console.WriteLine("Оператор введено не вiрно!");
-        break;
+    // Ініціалізація даних
+    Console.Write("Введіть перше число: ");
+    int first_number = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Введіть друге число: ");
+    int second_number = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Введіть одну з операцій (+, -, *, /): ");
+    string? math_operator = Console.ReadLine();
+
+    // Виведення результату
+    Console.Write($"{first_number} {math_operator} {second_number} = ");
+
+    // Обчислення результату
+    switch (math_operator)
+    {
+        case "+":
+            Console.WriteLine(first_number + second_number);
+            break;
+        case "-":
+            Console.WriteLine(first_number - second_number);
+            break;
+        case "*":
+            Console.WriteLine(first_number * second_number);
+            break;
+        case "/":
+            try
+            {
+                if (first_number % second_number == 0)
+                {
+                    Console.WriteLine(first_number / second_number);
+                }
+                else
+                {
+                    Console.WriteLine(Convert.ToDouble(first_number) / Convert.ToDouble(second_number));
+                }
+            }
+            // Виняток: ділення на 0
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Помилка!");
+                Console.WriteLine("На 0 ділити не можна!");
+            }
+            break;
+        default:
+            Console.WriteLine("Помилка!");
+            Console.WriteLine("Оператор введено невірно!");
+            break;
+    }
+}
+
+// Виняток: некоректний формат
+catch (FormatException)
+{
+    Console.WriteLine("Введено невірний формат числа!");
+}
+// Виняток переповнення
+catch (OverflowException)
+{
+    Console.WriteLine("Ви ввели занадто велике число!");
+}
+// Інші винятки
+catch (Exception)
+{
+    Console.WriteLine("Невідомий виняток!");
 }
