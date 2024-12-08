7223ab06ef97e023a912dedae210cc7bb964744a
Mykola <shanikolay@gmail.com>
2024-12-08 23:00:28 +0200

    Strings app v.1.0

---
+++ Strings_app/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Text;
+Console.OutputEncoding = Encoding.UTF8;
+
+// 10.1. Перевiрити ім’я та прізвище.
+
+Console.WriteLine($"10.1 Перевiрка ім’я та прізвища");
+Console.Write("Введіть ім'я та прізвище: ");
+string fullName = Console.ReadLine();
+
+int indexFirstName = fullName.IndexOf(" ");
+
+string letterName = fullName.Substring(0, 1);
+string letterFirstName = fullName.Substring(indexFirstName + 1, 1);
+
+if (indexFirstName == -1)
+{
+    Console.WriteLine("Некоректний ввід");
+}
+else
+{
+    if (letterName == letterFirstName)
+    {
+        Console.WriteLine("Прізвище починається на ту ж літеру, що і ім’я");
+    }
+    else
+    {
+        Console.WriteLine("Прізвище не починається на ту ж літеру, що і ім’я");
+    }
+}
+Console.WriteLine();
+
+// 10.2. Генерації текстового звіту.
+
+StringBuilder myReport = new StringBuilder();
+
+for (int i = 0; i < 5; i++)
+{
+    DateTime dateTime = DateTime.Now;
+    Console.WriteLine($"\t\t\t10.2 Звіт.\nВведіть подію №{i + 1}:");
+
+    myReport.Append($"{i + 1}. " +
+                    $"{dateTime.ToString("dd/MMM HH:mm")} - " +
+                    $"{Console.ReadLine()}\n");
+    Console.Clear();
+}
+
+Console.WriteLine($"\t\t\t10.2 Звіт.\n{myReport}");
+
+// 10.3. Видалення пробілiв.
+
+Console.WriteLine("10.3 Видалення пробілів.");
+Console.Write("Введіть декілька слів, розділяючи їх \',\': ");
+string withSpaces = Console.ReadLine();
+string withoutSpaces = withSpaces.Replace(" ", "");
+Console.WriteLine($"Після видалення пробілів: {withoutSpaces}");
