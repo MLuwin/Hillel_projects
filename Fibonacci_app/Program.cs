a358a3b6d0d717b71d57975c6c61ba0e7a19fdfe
Mykola <shanikolay@gmail.com>
2024-12-13 00:05:39 +0200

    Fibonacci app

---
+++ Fibonacci_app/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+
+// Числа Фібоначчі
+
+namespace Fibonacci_app
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            Console.Write("Введіть номер числа Фібоначчі.\nВхідний параметр: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Вихідне значення: {Fibonacci(number)}");
+        }
+
+        static int Fibonacci(int number)
+        {
+            if (number == 1)
+            {
+                return 0;
+            }
+            else if (number <= 3)
+            {
+                return 1;
+            }
+            else
+            {
+                return Fibonacci (number - 1) + Fibonacci(number - 2);
+            }
+        }
+    }
+}
---
+++ Gallows_app/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+Random random = new Random();
+
+// 7. Гра "Шибениця"
+
+Console.WriteLine("Вітаю у грі Шибениця!\nСпробуйте вгадати зишифроване слово!");
+
+List<string> gallows = ["шибениця", "собака", "табуретка", "квіти", "термос", "доміно", "карбюратор"];
+string word = gallows[random.Next(gallows.Count)];
+
+Console.WriteLine(word);
+
+Console.WriteLine("Кількість літер у слові: ");
+Console.WriteLine("Кількість можливих невірних спроб: ");
